60cf7f210529de735174f75bcd950875fe5761d7
Gowri Shankar Subramanian <Gowri_S01@ad.infosys.com>
2018-03-22 14:44:36 +0530

    changed verbiage

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -34,7 +34,7 @@ public class ApplicationController {
     public Result helloWorldJson() {
 
         SimplePojo simplePojo = new SimplePojo();
-        simplePojo.content = "Hello Heroku!!!";
+        simplePojo.content = "Hello World! Hello Json!";
 
         return Results.json().render(simplePojo);
 
