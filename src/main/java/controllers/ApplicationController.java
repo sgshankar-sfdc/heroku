9799c66c5ffb392212e5cfc4b978798d2ab31910
Gowri Shankar Subramanian <Gowri_S01@ad.infosys.com>
2018-03-22 12:56:57 +0530

    Changed language

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -30,19 +30,19 @@ public class ApplicationController {
         return Results.html();
 
     }
-    
+
     public Result helloWorldJson() {
-        
+
         SimplePojo simplePojo = new SimplePojo();
-        simplePojo.content = "Hello World! Hello Json!";
+        simplePojo.content = "Hello Heroku!!!";
 
         return Results.json().render(simplePojo);
 
     }
-    
+
     public static class SimplePojo {
 
         public String content;
-        
+
     }
 }
